80180c8b3a8420df1998c7890d8b8fa0cc561028
gabrielms201 <ricardogabrielmsruiz@gmail.com>
2021-07-08 01:43:33 -0300

    Criado a estrutura inicial do projeto. Implementado a entity User e criado, porém não configurado o UserController

---
+++ Cadastro.WebAPI/Cadastro.WebAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro.WebAPI.Models
+{
+	public class User
+	{
+		public string Id { get; set; }
+		public string FirstName { get; set; }
+		public string Surname { get; set; }
+		public int Age { get; set; }
+		public DateTime CreationDate { get; set; }
+	}
+}
