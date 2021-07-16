e8194dc378ac3778a94f5d29cf4bd0b5f0ca9ca7
gabrielms201 <ricardogabrielmsruiz@gmail.com>
2021-07-16 01:24:15 -0300

    Adicionado a estrutura do aplicativo e a configuração para aceitar a api via localhost

---
+++ Cadastro.WebAPI/Cadastro.App/Cadastro.App/ApiService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace Cadastro.App
+{
+	class ApiService
+	{
+		public static string BaseAddress =
+		DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5001" : "http://localhost:8090";
+		public static string Users = $"{BaseAddress}/api/Users/";
+	}
+}
---
+++ Cadastro.WebAPI/Cadastro.App/Cadastro.App/UserRegisterPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cadastro.App
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class UserRegisterPage : ContentPage
+	{
+		public UserRegisterPage()
+		{
+			InitializeComponent();
+			Title = "Registrar Usuário";
+		}
+
+		private void SendRegister(object sender, EventArgs e)
+		{
+			(sender as Button).Text = age.Text;
+		}
+	}
+}
