2e16f17a87655df4cc063d9599896d3628dc3fea
gabrielms201 <ricardogabrielmsruiz@gmail.com>
2021-07-16 18:44:43 -0300

    Finalizado a criação do aplicativo mobile

---
+++ Cadastro.WebAPI/Cadastro.App/Cadastro.App/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadastro.App.Entities
+{
+	class User
+	{
+		public string Id { get; private set; }
+		public string FirstName { get; set; }
+		public string Surname { get; set; }
+		public int? Age { get; set; }
+		public DateTime CreationDate { get; private set; }
+	}
+}
